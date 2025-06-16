8abfcad6b5bbd1038b4bb54ce5dbcfd30dbda225
Henrique <71376676+henrique-cavaleiro@users.noreply.github.com>
2025-06-16 10:10:03 +0200

    Update cryptoAPI.js

--- express-docker/src/cryptoAPI.js
+++ express-docker/src/cryptoAPI.js
@@ -4,22 +4,22 @@ export async function fetchCryptoPrices(currency = "USD") {
     try {
         const btcUrl = `https://api.coinbase.com/v2/prices/BTC-${currency}/spot`;
         const ethUrl = `https://api.coinbase.com/v2/prices/ETH-${currency}/spot`;
-        const dogeUrl = `https://api.coinbase.com/v2/prices/doge-${currency}/spot`;
+        const dogeUrl = `https://api.coinbase.com/v2/prices/doge-${currency}/spot`;                 // examen
         const [btcData, ethData, dogeData] = await Promise.all([
             fetch(btcUrl).then(res => res.json()),
             fetch(ethUrl).then(res => res.json()),
-            fetch(dogeUrl).then(res => res.json())
+            fetch(dogeUrl).then(res => res.json())          // examen
             
         ]);
 
         return {
             BTC: btcData.data.amount,
             ETH: ethData.data.amount,
-            DOGE: dogeData.data.amount
+            DOGE: dogeData.data.amount          // examen
 
         };
     } catch (error) {
         console.error("Error fetching prices:", error);
-        return { BTC: null, ETH: null, Doge: null };
+        return { BTC: null, ETH: null, Doge: null };            // examen
     }
 }
